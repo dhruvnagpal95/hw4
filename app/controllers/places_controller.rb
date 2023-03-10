ae405fc9d1b6033a97afa8cd81d4055c4e7ae8d3
dhruvnagpal95 <dhruv.nagpal@kellogg.northwestern.edu>
2023-03-10 22:12:04 +0000

    images

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -6,7 +6,12 @@ class PlacesController < ApplicationController
 
   def show
     @place = Place.find_by({ "id" => params["id"] })
-    @posts = Post.where({ "place_id" => @place["id"] })
+    if @current_user
+      @posts = Post.where({ "place_id" => @place["id"] , "user_id" => @current_user["id"]})
+    else
+      flash["notice"] = "Please login first"
+      redirect_to "/login"
+    end  
   end
 
   def new
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,7 @@ class PostsController < ApplicationController
     @post.place_id = params["place_id"]
   end
 
+  
   def create
     @post = Post.new
     @post["title"] = params["post"]["title"]
@@ -12,6 +13,7 @@ class PostsController < ApplicationController
     @post["posted_on"] = params["post"]["posted_on"]
     @post["place_id"] = params["post"]["place_id"]
     @post["user_id"] = @current_user["id"]
+    @post["image"] = params["post"]["image"]
     @post.save
     redirect_to "/places/#{@post["place_id"]}"
   end
